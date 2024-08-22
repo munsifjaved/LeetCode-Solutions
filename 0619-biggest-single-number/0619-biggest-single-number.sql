709f3dd9047199c4e90d3752533fa6dd86f1fa30
Munsif Jaweed <140373095+munsifjaved@users.noreply.github.com>
2024-08-22 10:43:58 +0500

    Time: 315 ms (82.43%), Space: 0B (100.00%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT COALESCE(
+    (SELECT TOP 1 num
+     FROM MyNumbers
+     GROUP BY num
+     HAVING COUNT(num) = 1
+     ORDER BY num DESC), NULL) AS num;
